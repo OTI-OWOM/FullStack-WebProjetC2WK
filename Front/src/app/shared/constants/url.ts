6fe394660371ab49392631f5a9aa1b2fe79041db
Mileristovski <65347276+Mileristovski@users.noreply.github.com>
2023-10-28 20:19:00 +0200

    More fixes

--- Front/src/app/shared/constants/url.ts
+++ Front/src/app/shared/constants/url.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://__API_URL__';
 
 export const REGISTER_URL = `${BASE_URL}/register`;
 export const LOGIN_URL = `${BASE_URL}/login`;
