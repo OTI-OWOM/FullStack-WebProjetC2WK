4aaa2e6cf9009664b95c66ff3b697a1632687824
Mileristovski <65347276+Mileristovski@users.noreply.github.com>
2023-12-09 20:26:41 +0100

    Deployment ready v1

--- Front/src/app/shared/constants/url.ts
+++ Front/src/app/shared/constants/url.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://__API_URL__';
+const BASE_URL = 'https://mileristovski.fr/api/';
 
 export const REGISTER_URL = `${BASE_URL}/register`;
 export const LOGIN_URL = `${BASE_URL}/login`;
