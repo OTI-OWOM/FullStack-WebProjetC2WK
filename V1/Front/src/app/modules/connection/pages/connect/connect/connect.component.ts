60ac8f1e70ae4e88ac6343ea9b5576a8a5da0e08
mile <mile@test.com>
2024-05-22 15:33:22 +0200

    [FIX] - Fixed connection bugs

--- V1/Front/src/app/modules/connection/pages/connect/connect/connect.component.ts
+++ V1/Front/src/app/modules/connection/pages/connect/connect/connect.component.ts
@@ -4,39 +4,43 @@ import { Subscription } from 'rxjs';
 import { AuthService } from '@core/services/auth.service';
 
 @Component({
-  selector: 'app-connect',
-  templateUrl: './connect.component.html',
-  styleUrls: ['./connect.component.scss']
+    selector: 'app-connect',
+    templateUrl: './connect.component.html',
+    styleUrls: ['./connect.component.scss']
 })
 export class ConnectComponent implements OnDestroy {
-  private subscription: Subscription = new Subscription();
+    private subscription: Subscription = new Subscription();
 
-  message!: string;
+    message!: string;
 
-  constructor(
-      private auth: AuthService,
-      private router: Router,
-  ) {}
+    constructor(
+        private auth: AuthService,
+        private router: Router,
+    ) { }
 
 
-  validation(Email: string, Password: string): void {
-      if (Email && Password) {
-          const loginSubscription = this.auth.loginUser(Email, Password).subscribe({
-                      next: () => {
-                          this.router.navigateByUrl('/product');
-                      },
-                      error: async (err: any) => {
-                          this.message = "Login or password incorrect.";
-                      },
-                  });
-          
-          this.subscription.add(loginSubscription);         
-      } else {
-          this.message = Email ? "You need to provide a password!" : "You need to provide an Email!"
-      }
-  }
+    validation(Email: string, Password: string): void {
+        if (Email && Password) {
+            const loginSubscription = this.auth.loginUser(Email, Password).subscribe({
+                next: () => {
+                    this.router.navigateByUrl('/product');
+                },
+                error: (err: any) => {
+                    if (err && err.error && err.error.message) {
+                        this.message = err.error.message;
+                    } else {
+                        this.message = 'An unknown error occurred';
+                    }
+                },
+            });
 
-  ngOnDestroy() {
-      this.subscription.unsubscribe();
-  }
+            this.subscription.add(loginSubscription);
+        } else {
+            this.message = Email ? "You need to provide a Password!" : "You need to provide an Email!"
+        }
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
