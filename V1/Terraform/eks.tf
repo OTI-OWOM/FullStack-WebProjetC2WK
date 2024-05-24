acee2fe27b0ac8781a386d2609297a60de2de104
mile <mile@test.com>
2024-05-24 10:07:53 +0200

    [FIX] - Synchronize

--- V1/Terraform/eks.tf
+++ V1/Terraform/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "18.29.0"
 
   cluster_name    = "my-eks"
-  cluster_version = "1.23"
+  cluster_version = "1.28"
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
@@ -17,6 +17,17 @@ module "eks" {
     disk_size = 50
   }
 
+  node_security_group_additional_rules = {
+    cluster_to_node = {
+      description      = "Cluster to ingress-nginx webhook"
+      protocol         = "-1"
+      from_port        = 8443
+      to_port          = 8443
+      type             = "ingress"
+      source_cluster_security_group = true
+    }
+  }
+
   eks_managed_node_groups = {
     general = {
       desired_size = 3
@@ -27,9 +38,28 @@ module "eks" {
         role = "general"
       }
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.xlarge"]
       capacity_type  = "ON_DEMAND"
     }
+
+    # spot = {
+    #   desired_size = 1
+    #   min_size     = 1
+    #   max_size     = 10
+
+    #   labels = {
+    #     role = "spot"
+    #   }
+
+    #   taints = [{
+    #     key    = "market"
+    #     value  = "spot"
+    #     effect = "NO_SCHEDULE"
+    #   }]
+
+    #   instance_types = ["t3.micro"]
+    #   capacity_type  = "SPOT"
+    # }
   }
 
   tags = {
